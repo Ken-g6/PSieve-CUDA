b5242fd2cbd8e2972f6fb4dc20354846235d7fef
Ken_g6 <kbrazier@gmail.com>
2010-03-17 10:00:32 -0600

    BOINC changes to config files

--- pps/make-zips.sh
+++ pps/make-zips.sh
@@ -3,6 +3,6 @@ apppath=pps
 rm -f $appname.zip $appname-src.zip
 # Save CHANGES.txt
 git log | grep "^\(    \|Date:\)" > CHANGES.txt
-zip -9 $appname.zip $appname-x86-linux $appname-x86-linux-sse2 $appname-x86_64-linux $appname-x86-windows.exe $appname-x86-windows-sse2.exe $appname-x86_64-windows.exe README.txt CHANGES.txt $appname.sh $appname.bat *config.txt license.txt
+zip -9 $appname.zip $appname-x86-linux $appname-boinc-x86-linux $appname-x86_64-linux $appname-boinc-x86_64-linux $appname-boinc-x86-windows.exe $appname-x86-windows.exe $appname-boinc-x86_64-windows.exe $appname-x86_64-windows.exe README.txt CHANGES.txt $appname.sh $appname.bat *config.txt license.txt
 cd ..
 zip -9 $apppath/$appname-src.zip Makefile *.txt *.[ch] $apppath/*.[ch] $apppath/*.cu $apppath/README.txt $apppath/CHANGES.txt $apppath/*config.txt $apppath/make-*.*[ht] $apppath/$appname.sh $apppath/$appname.bat
