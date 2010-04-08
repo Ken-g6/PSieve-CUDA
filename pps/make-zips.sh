7b563fa318056a33f6243374af71f18045b22057
Ken_g6 <kbrazier@gmail.com>
2010-04-07 18:56:06 -0600

    make-zips aborts on un-committed git changes.

--- pps/make-zips.sh
+++ pps/make-zips.sh
@@ -1,6 +1,12 @@
 appname=ppsieve-cuda
 apppath=pps
 rm -f $appname.zip $appname-src.zip
+# Make sure everything's committed.
+status=`git status | grep nothing | wc -l`
+if [ $status == "0" ] ; then
+	git status
+	exit
+fi
 # Save CHANGES.txt
 git log | grep "^\(    \|Date:\)" > CHANGES.txt
 zip -9 $appname.zip $appname-x86-linux $appname-boinc-x86-linux $appname-x86_64-linux $appname-boinc-x86_64-linux $appname-boinc-x86-windows.exe $appname-x86-windows.exe $appname-boinc-x86_64-windows.exe $appname-x86_64-windows.exe README.txt CHANGES.txt $appname.sh $appname.bat *config.txt license.txt
