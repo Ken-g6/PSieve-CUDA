7b7b50e9d3618be12108c6374e941e7b11720b9a
ibethune <iain@pyramid-productions.net>
2010-07-07 20:45:29 +0100

    Mac Builds (64 & 32 bit)

---
+++ pps/make-mac32.sh
@@ -0,0 +1,7 @@
+appname=primegrid_ppsieve_1.00_i686-apple-darwin__cuda23
+BOINC_DIR=../../../boinc/boinc-6.7.4
+BOINC_API_DIR=$BOINC_DIR/api
+BOINC_LIB_DIR=$BOINC_DIR/lib
+BOINC_LOAD_LIBS="-I$BOINC_DIR -I$BOINC_LIB_DIR -I$BOINC_API_DIR -L$BOINC_DIR -L$BOINC_LIB_DIR -L$BOINC_API_DIR"
+
+MACOSX_DEPLOYMENT_TARGET=10.5 nvcc -arch=sm_11 --ptxas-options=-v -O3 -m32 -Xcompiler "-arch i386 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5" -Xlinker "-syslibroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5" -D_GNU_SOURCE -DUSE_BOINC -DNDEBUG -D_REENTRANT -I. -I.. -o $appname $BOINC_LOAD_LIBS ../main.c ../sieve.c ../clock.c ../putil.c cuda_sleep_memcpy.cu appcu.cu app.c -lm -lpthread -lboinc_api -lboinc -lcuda
