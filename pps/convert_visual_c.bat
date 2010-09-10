326728a364a1ec5fd8ff5abf9f8db1b3a1324a5f
Ken Brazier <kbrazier@gmail.com>
2010-09-10 12:18:42 -0600

    Better guess/upper limit on -m

--- pps/convert_visual_c.bat
+++ pps/convert_visual_c.bat
@@ -3,5 +3,15 @@ copy ..\*.c .
 copy ..\*.cpp .
 copy ..\*.h .
 copy ..\vc\*.* .
+perl cltoh.pl appcl.cl > temp.h
+if errorlevel 1 goto perlerr
+copy appcl_start.h+temp.h > appcl.h
 cd ..
 start ppsieve-cuda.sln
+start ppsieve-cl.sln
+goto end
+
+:perlerr
+echo Error!  Please install a Perl interpreter to continue.
+echo Try http://strawberryperl.com/
+:end
